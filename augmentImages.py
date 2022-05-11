6e22a169713c0e150470ed764739c9880654e7fa
elibrowne <eli.browne.23@bishops.com>
2022-05-11 11:57:46 -0700

    Started code to augment light blue computer masks to other colors

--- augmentImages.py
+++ augmentImages.py
@@ -1,27 +1,32 @@
-# Because I've decided to run the code focusing on two classes (correct mask or
-# not) rather than three classes (correct, incorrect, nonexistent), I've tried
-# to make up for the data imbalance by duplicating and augmenting the correct
-# picutres.
+# Augmenting my data in order to use a more diverse dataset:
+# This program will be run on the computer-generated mask dataset, because 
+# these masks are all the exact same blue color. I will change some of them to 
+# black, red, or a darker blue, in order to improve the quality of the CNN. 
 
-# Imports: reading images in
-import os
-from os import listdir
-# Imports: saving and modifying images
-import cv2
-# Imports: other methods
-import random # used for random rotation
+import cv2 # OpenCV - image editing
+import argparse # Argparse - testing with different images
+import numpy as np # NumPy - arrays, etc. 
 
-folder_dir = "mask-present-dataset/with_passing_mask"
-slay = 0
-for imageName in os.listdir(folder_dir):
-	# Load the image in
-	image = cv2.imread("mask-present-dataset/with_passing_mask/" + imageName)
-	# TODO FIX THIS!!
-	# Rotate the image slightly by a random amount
-	rotationMatrix = cv2.getRotationMatrix2D((64, 64), random.randint(-45, 45), 1.0)
-	image = cv2.warpAffine(image, rotationMatrix, (128, 128))
-	# Zoom in on the image to cut out black space
-	image = image[20:110, 20:110]
-	# Resize image to 128 x 128 for the dataset
-	image = cv2.resize(image, (128, 128), interpolation = cv2.INTER_AREA)
-	cv2.imwrite('mask-present-dataset/with_passing_mask/augmented_' + imageName, image)
+# Basic introductory code: ask for an image (to test the data editing on)
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required = True, help = "Path to the image")
+args = vars(ap.parse_args())
+
+image = cv2.imread(args["image"])
+hsvImage = cv2.cvtColor(image, cv2.COLOR_BGR2HSV) # convert to HSV to edit color more easily
+cv2.imshow("Original Image", image)
+cv2.waitKey(0)
+
+# Here, we can try to establish limits for which colors are edited by this program.
+# This was done using the "mask swatch" image (should be included) and varying the
+# average hue, which was determined to be hue 199.
+# Note: in OpenCV, hue is done from 0 to 180, not 0 to 360. I used 188 and 206.
+maskBlueStart = np.array([94, 0, 0]) # darkest mask color swatched from dataset
+maskBlueEnd = np.array([107, 255, 255]) # lightest mask color swatched from dataset
+
+maskOfMask = cv2.inRange(hsvImage, maskBlueStart, maskBlueEnd)
+
+image[maskOfMask > 0] = (0, 0, 255) # highlight the selected region as red
+
+cv2.imshow("Masked Image", image)
+cv2.waitKey(0)
